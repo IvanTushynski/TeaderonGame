dfa947caa7199fa88651d47e523ec393911d8669
Ivan Tushynski <tushinski.ivan@gmail.com>
2022-09-15 22:05:59 +0300

    update

--- src/com/gameclasses/MainGame.java
+++ src/com/gameclasses/MainGame.java
@@ -11,14 +11,13 @@ public class MainGame {
         Warrior gameWarrior = new Warrior();
         Mage gameMage = new Mage();
 
-        //не работает
-        System.out.println("Enter Player Class - Mage or Warrior");
-        String nameClass = scanClass.nextLine();
+        System.out.println("Select Player Class - Mage(1) or Warrior(2)");
+        int nameClass = scanClass.nextInt();
 
-        if (nameClass == "Warrior"){
-            System.out.println("Your Selected Class: Warrior");
-        } else if (nameClass == "Mage") {
-            System.out.println("Your Selected Class: Mage");
+        if (nameClass == 1){
+            System.out.println("You Selected Class [Mage]");
+        } else if (nameClass == 2) {
+            System.out.println("You Selected Class [Warrior]");
         } else {
             System.out.println("No Such Class...");
         }
@@ -28,7 +27,7 @@ public class MainGame {
         int expPerLevel = 100;
 
         for (int iWarrior = 1; iWarrior <= playerLevel; iWarrior++){
-            if (playerLevel <= 20) {
+            if (playerLevel <= 20 && nameClass == 2) {
                 gameWarrior.hpWarrior = gameWarrior.hpWarrior + 10;
                 gameWarrior.mpWarrior = gameWarrior.mpWarrior + 10;
                 gameWarrior.attackWarrior = gameWarrior.attackWarrior + 10;
@@ -44,8 +43,24 @@ public class MainGame {
                 System.out.println("Defence-" + gameWarrior.defenceWarrior);
                 System.out.println("Experience Per Level:" + expPerLevel);
                 System.out.println("------------------");
+            } else if (playerLevel <= 20 && nameClass == 1) {
+                gameMage.hpMage = gameMage.hpMage + 10;
+                gameMage.mpMage = gameMage.mpMage + 10;
+                gameMage.attackMage = gameMage.attackMage + 10;
+                gameMage.defenceMage = gameMage.defenceMage + 5;
+
+                expPerLevel = expPerLevel + 100;
+
+                System.out.println(gameMage.name);
+                System.out.println("Player Level:" + iWarrior);
+                System.out.println("HP-" + gameMage.hpMage);
+                System.out.println("MP-" + gameMage.mpMage);
+                System.out.println("Attack-" + gameMage.attackMage);
+                System.out.println("Defence-" + gameMage.defenceMage);
+                System.out.println("Experience Per Level:" + expPerLevel);
+                System.out.println("------------------");
             }else {
-                System.out.println("Max Level 20");
+                System.out.println("Undefined Level of Class...");
                 break;
             }
         }
