43dbb6c5246afef3a8bd42ee5e73f7f6e3d8cf6e
IvanTushinski <tushinski.ivan@gmail.com>
2022-09-28 23:31:26 +0300

    update

--- src/com/gameclasses/MainGame.java
+++ src/com/gameclasses/MainGame.java
@@ -6,52 +6,63 @@ import java.util.Scanner;
 
 public class MainGame {
     public static void main(String[] args) {
-
+        Scanner gameScan = new Scanner(System.in);
         Warrior warOne = new Warrior();
         goblins goblinsFight = new goblins();
         warOne.stateGenAttackWarrior(warOne.attackWarrior);
         warOne.stateGenDefenceWarrior(warOne.defenceWarrior);
         warOne.stateGenHpWarrior(warOne.hpWarrior);
         warOne.stateGenMpWarrior(warOne.mpWarrior);
-
+        warOne.levelsWarrior = 1;
         warOne.swordTruFalseOne = false;
 
-
-        System.out.println("Your Attack " + warOne.stateGenAttackWarrior(warOne.attackWarrior));
-
-
-        //бой с гоблином 1-----------------------------------------------------
-        System.out.println("Select Fight-1 Or Leave-2");
-        Scanner gameScan = new Scanner(System.in);
-        int fight = gameScan.nextInt();
-        if (fight == 1) {
-            //что бы работало понижение жизней у перса и противника
-            warOne.YourRemainingLeaves = warOne.stateGenDefenceWarrior(warOne.defenceWarrior) + warOne.stateGenHpWarrior(warOne.hpWarrior);
-            warOne.EnemyRemainingLeaves = goblinsFight.hpGoblin1;
-            //сам цикл боя
-            for (int i = 1; i > 0; i++) {
-                System.out.println("Your Hero |" + warOne.name + "| Enemy |" + goblinsFight.nameGoblin1);
-                System.out.println("Select Attack-1\n Select Leave-2");
-                int Attack = gameScan.nextInt();
-                if (Attack == 1) {
-                    if (warOne.YourRemainingLeaves > 0 || warOne.EnemyRemainingLeaves > 0) {
-                        warOne.fightWithGoblin1();
-                    } else if (warOne.EnemyRemainingLeaves <= 0) {
-                        System.out.println("You Win Killing " + goblinsFight.nameGoblin1);
-                        break;
-                    } else if (warOne.YourRemainingLeaves <= 0) {
-                        System.out.println("You Lose... Killed By " + goblinsFight.nameGoblin1);
-                        break;
-                    }
-                } else if (Attack == 2) {
-                    System.out.println("You Are Living In Combat...");
+        //главное меню
+        for (int m = 1; m > 0; m++) {
+            System.out.println("1 - Hero Info | 2 - Monsters");
+            int menu = gameScan.nextInt();
+            switch (menu) {
+                case 1 :
+                    System.out.println("--------------------|Hero Info|--------------------");
+                    System.out.println("Attack " + warOne.stateGenAttackWarrior(warOne.attackWarrior));
+                    System.out.println("Defence " + warOne.stateGenDefenceWarrior(warOne.defenceWarrior));
+                    System.out.println("Hp " + warOne.stateGenHpWarrior(warOne.hpWarrior));
+                    System.out.println("Mp " + warOne.stateGenMpWarrior(warOne.mpWarrior));
+                    System.out.println("Level " + warOne.levelsWarrior);
                     break;
-                } else {
-                    System.out.println("Select Attack-1\n Select Leave-2");
-                }
+                case 2:
+                    //бой с гоблином 1-----------------------------------------------------
+                    System.out.println("Select Fight-1 Or Leave-2");
+                    int fight = gameScan.nextInt();
+                    if (fight == 1) {
+                        //что бы работало понижение жизней у перса и противника
+                        warOne.YourRemainingLeaves = warOne.stateGenDefenceWarrior(warOne.defenceWarrior) + warOne.stateGenHpWarrior(warOne.hpWarrior);
+                        warOne.EnemyRemainingLeaves = goblinsFight.hpGoblin1;
+                        //сам цикл боя
+                        for (int i = 1; i > 0; i++) {
+                            System.out.println("Your Hero |" + warOne.name + "| Enemy |" + goblinsFight.nameGoblin1);
+                            System.out.println("Select Attack-1\n Select Leave-2");
+                            int Attack = gameScan.nextInt();
+                            if (Attack == 1) {
+                                if (warOne.YourRemainingLeaves > 0 && warOne.EnemyRemainingLeaves > 0) {
+                                    warOne.fightWithGoblin1();
+                                } else if (warOne.EnemyRemainingLeaves <= 0) {
+                                    System.out.println("You Kill " + goblinsFight.nameGoblin1 + "And Obtained " + goblinsFight.expGoblin1 + "XP");
+                                    break;
+                                } else if (warOne.YourRemainingLeaves <= 0) {
+                                    System.out.println("You Lose... Killed By " + goblinsFight.nameGoblin1);
+                                    break;
+                                }
+                            } else if (Attack == 2) {
+                                System.out.println("You Are Living In Combat...");
+                                break;
+                            } else {
+                                System.out.println("Select Attack-1\n Select Leave-2");
+                            }
+                        }
+                    } else if (fight == 2) {
+                        System.out.println("You Are Living In Combat...");
+                    }
             }
-        } else if (fight == 2) {
-            System.out.println("You Are Living In Combat...");
         }
     }
 }
